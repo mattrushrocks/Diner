3cc17cd6a0fa853c8d75a582a0e33bd8846a49b6
Matt Rushforth <matthewrushforth12@gmail.com>
2025-12-05 11:23:35 -0700

    Let's try this again

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-// update the year in the footer
-const rightNow = new Date();
-document.querySelector('#year').textContent = rightNow.getFullYear();
+//inject current year in footer
+const variableOne = new Date();
+document.querySelector('#year').textContent = variableOne.getFullYear();
+
+
+//new stuff here
+
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerButton")
+
+x.onclick = toggleMenu;
